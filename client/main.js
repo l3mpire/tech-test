db2103ff2eb921c0dbf0591e16548322eaa2504b
mel-mouk <mathilde@elmouktafi.fr>
2022-05-10 15:04:49 +0200

    Tech test boilerplate

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+
+import './main.html';
+
+Template.appBlock.onCreated(function helloOnCreated() { });
+
+Template.appBlock.helpers({ });
+
+Template.appBlock.events({ });
